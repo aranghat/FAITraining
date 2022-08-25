4732d404be8d251acff9a91f1ed22da14d172802
Aranghat <aranghat@outlook.com>
2022-08-25 10:42:32 +0530

    Added Interface

---
+++ InterfaceBasics/IAudioService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBasics
+{
+    public interface IDeviceInfoService
+    {
+        string DeviceName { get;  }
+        string SerialNo { get;  }
+    }
+
+    public interface IAudioService
+    {
+        void PlayAudio(string fileName);
+        void StopAudio();
+    }
+
+    public interface IVideoService
+    {
+        void PlayMedia(string fileName);
+        void StopMedia();
+    }
+
+    public class MyTunes : IDeviceInfoService, IAudioService
+    {
+        private string fileName = "";
+
+        public string DeviceName { get { return "MYTUNES ";  } }
+
+        public string SerialNo { get { return "SN:123456"; } }
+
+        public void PlayAudio(string fileName)
+        {
+            this.fileName = fileName;
+            Console.WriteLine($"MyTunes : Playing audio from file {fileName}");
+        }
+
+        public void StopAudio()
+        {
+            if (fileName.Length > 0)
+            {
+                Console.WriteLine($"My Tunes :Stopping file {fileName}");
+                fileName = "";
+
+            }
+            else
+                Console.WriteLine("No file is playing");
+        }
+    }
+
+    public class MyPhone : IAudioService, IVideoService
+    {
+        private string fileName = "";
+        public void PlayAudio(string fileName)
+        {
+            this.fileName = fileName;
+            Console.WriteLine($"MyPhone : Playing audio from file {fileName}. Touch to stop");
+        }
+
+        public void PlayMedia(string fileName)
+        {
+            Console.WriteLine($"Playing Video {fileName}");
+        }
+
+        public void StopAudio()
+        {
+            if (fileName.Length > 0)
+            {
+                Console.WriteLine($"My Tunes :Stopping file {fileName}");
+                fileName = "";
+
+            }
+            else
+                Console.WriteLine("No file is playing");
+        }
+
+        public void StopMedia()
+        {
+            Console.WriteLine($"Stopping Video {fileName}");
+        }
+    }
+
+    public class MyVrHeadSet : IAudioService, IVideoService
+    {
+        public void PlayAudio(string fileName)
+        {
+            Console.WriteLine("Playing in VR");
+        }
+
+        public void PlayMedia(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopAudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopMedia()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterfaceBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file name : ");
+            string fileName = Console.ReadLine();
+
+            Console.Write("Where do you need to play : ");
+            string deviceName = Console.ReadLine();
+
+            IAudioService device = PickAudioDevice(deviceName);
+            device.PlayAudio(fileName);
+        }
+        static IAudioService PickAudioDevice(string deviceName)
+        {
+            IAudioService audioDevice = null;
+
+            switch(deviceName)
+            {
+                case "MyTunes": audioDevice = new MyTunes();break;
+                case "MyPhone": audioDevice = new MyPhone(); break;
+                case "VR": audioDevice = new MyVrHeadSet(); break;
+            }
+
+            return audioDevice;
+        }
+    }
+}
