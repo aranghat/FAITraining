44455f08dea8c1d24091228ee32d05c6e04b3f4c
Aranghat <aranghat@outlook.com>
2022-09-13 12:03:45 +0530

    Added Cookies

---
+++ AspNetStateManagement/CookieBasics.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CookieBasics.aspx.cs" Inherits="AspNetStateManagement.CookieBasics" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>
+                <asp:Label ID="lblNote" runat="server" Text=""></asp:Label></h1>
+            <asp:TextBox ID="txtNote" placeholder="Keep a note to remember" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSaveNote" runat="server" Text="Save Note" OnClick="btnSaveNote_Click" /></div>
+    </form>
+</body>
+</html>
---
+++ AspNetStateManagement/CookieBasics.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetStateManagement
+{
+    public partial class CookieBasics : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.Cookies["MyNote"] != null)
+            {
+                lblNote.Text = $"Gentle Reminder : {Request.Cookies["MyNote"].Value}";
+            }
+        }
+
+        protected void btnSaveNote_Click(object sender, EventArgs e)
+        {
+            if(txtNote.Text.Length > 0)
+            {
+                var cookie = new HttpCookie("MyNote");
+                cookie.Value = txtNote.Text;
+                cookie.Expires = DateTime.Now.AddDays(7);
+
+                Response.Cookies.Add(cookie);
+            }
+        }
+    }
+}
